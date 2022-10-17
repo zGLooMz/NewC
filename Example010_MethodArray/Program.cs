100abda89d2bf2183aa01415fed83d83e3224859
Alexandr Zybailo <zero-pushkin@mail.ru>
2022-10-17 16:14:58 +0300

    Ad Example010

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 1, 8, 31, 4, 51, 6, 17, 8 };
+int n = array.Length;
+int find = 8;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+
+    index++;
+}
